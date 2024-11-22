2390dc55037cbc463a98b870fbe42f6ec18c2319
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-22 17:16:21 +0300

    Finish upto currency button

--- Gebeya/src/components/Navbar.jsx
+++ Gebeya/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import {FaUser} from 'react-icons/fa'
+import {FaUser} from 'react-icons/fa';
 import {RiArrowDownSLine} from 'react-icons/ri';
+import { IoSearch } from "react-icons/io5";
 import {NavLink} from 'react-router-dom'
-import Logo from '../assets/Logo.png'
+import Logo from '../assets/Logo.png';
+import Flag from '../assets/Flag.png';
 
 function Navbar() {
   return (
-    <div className="flex items-center space-x-5 w-full p-4 text-[#1E1E1E]">
+    <div className="flex items-center space-x-5 w-full p-4 text-[#1E1E1E] text-base">
       <img src={Logo} alt="Logo" className="h-10"/>
       <div className="flex items-center">
         <FaUser/>Register/Sign In<RiArrowDownSLine/>
@@ -19,8 +21,12 @@ function Navbar() {
             <NavLink to='/' className='flex flex-row items-center'>More<RiArrowDownSLine/></NavLink>
         </div>
         <div className="flex items-center">
-          <input type="text" placeholder="Search" className="p-2 border rounded-l-2xl"/>
-          <button className="p-2 bg-primary text-white rounded-r-2xl">Search</button>
+          <input type="text" placeholder={`${<IoSearch />}Search`} className="h-10 p-2 border rounded-l-2xl"/>
+          <button className="h-10 p-2 bg-primary text-white rounded-r-2xl"><IoSearch/></button>
+        </div>
+        <div className="flex items-center">
+          <img src={Flag} alt="Flag" className="h-6 border rounded-full"/>
+          <p className='flex items-center hover:cursor-pointer'>EN/Currency <RiArrowDownSLine/></p>
         </div>
     </div>
   )
