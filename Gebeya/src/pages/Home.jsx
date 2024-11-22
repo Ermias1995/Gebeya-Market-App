9f57bb8f1981d6de8633bacc1910b7aa5349e6db
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-22 22:11:28 +0300

    complete first part of home

--- Gebeya/src/pages/Home.jsx
+++ Gebeya/src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import Navbar from "../components/Navbar"
+import Ellipse_5 from '../assets/Ellipse_5.png'
 
 function Home() {
   return (
     <div>
-        <h1>Home Page</h1>
+        <div className="grid grid-cols-3 bg-primary w-full h-[769px]">
+          <div className="flex flex-col items-center justify-around">
+            <h1 className="font-normal text-9xl text-white">Gebeya</h1>
+            <div className="flex items-center justify-center w-[260px] h-[260px] rounded-full">
+              <img src={Ellipse_5} alt="Ellipse_5" />
+            </div>
+          </div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
     </div>
   )
 }
