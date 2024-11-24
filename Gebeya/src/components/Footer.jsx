d3528bf6f9b0c3b543d49e08e6f518771e87a524
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-24 22:26:03 +0300

    add footer icon

--- Gebeya/src/components/Footer.jsx
+++ Gebeya/src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import Logo from '../assets/Logo.png';
+import footer_icon from '../assets/footer_icon.png';
 function Footer() {
   return (
-    <div className='flex justify-stretch bg-primary min-h-[423px]'>
+    <div className='flex justify-stretch bg-primary min-h-[423px] mt-40'>
       <div id='Logo side' className="flex items-center justify-center">
         <div className="flex">
-          <img src={Logo} alt="Logo"/>
-          <img src={Logo} alt="Logo"/>
+          <img src={footer_icon} alt="Logo"/>
         </div>
       </div>
       <div className="Link-side">
