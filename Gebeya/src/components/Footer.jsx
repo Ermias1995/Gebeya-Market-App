8832e03509f9885c832fe13340494904b158ef27
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-24 22:18:37 +0300

    start footer logo

--- Gebeya/src/components/Footer.jsx
+++ Gebeya/src/components/Footer.jsx
@@ -1,6 +1,17 @@
+import Logo from '../assets/Logo.png';
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='flex justify-stretch bg-primary min-h-[423px]'>
+      <div id='Logo side' className="flex items-center justify-center">
+        <div className="flex">
+          <img src={Logo} alt="Logo"/>
+          <img src={Logo} alt="Logo"/>
+        </div>
+      </div>
+      <div className="Link-side">
+
+      </div>
+    </div>
   )
 }
 export default Footer
