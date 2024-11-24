9e93559fa403ec0d4cedbd368b774005c984de2d
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-24 23:10:45 +0300

    Finish footer

--- Gebeya/src/components/Footer.jsx
+++ Gebeya/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import footer_icon from '../assets/footer_icon.png';
 
 function Footer() {
   return (
+    <>
     <div className='flex justify-between bg-primary min-h-[423px] mt-40'>
       <div id='Logo side' className="flex flex-col items-center justify-center">
         <div className="flex ml-14">
@@ -14,17 +15,29 @@ function Footer() {
           <button className="text-white text-3xl"><FaXTwitter/></button>
         </div>
       </div>
-      <div id='Link-side' className="grid grid-cols-3 text-white">
+      <div id='Link-side' className="grid grid-cols-3 text-white mr-14 gap-6">
         <div className="flex flex-col items-start justify-center">
           <h1 className="font-bold text-2xl">About</h1>
           <button className="text-xl">About Us</button>
           <button className="text-xl">News & Blog</button>
           <button className="text-xl">Location</button>
         </div>
-        <div className=""></div>
-        <div className=""></div>
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="font-bold text-2xl">Products</h1>
+          <button className="text-xl">Pricing</button>
+          <button className="text-xl">Store</button>
+          <button className="text-xl">Features</button>
+        </div>
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="font-bold text-2xl">Discover</h1>
+          <button className="text-xl">Contact Us</button>
+          <button className="text-xl">Customers</button>
+          <button className="text-xl">Affilates</button>
+        </div>
       </div>
     </div>
+    <div className="bg-white h-8"></div>
+    </>
   )
 }
 export default Footer
