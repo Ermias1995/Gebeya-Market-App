ac15d602c95742353fc3cf73d831a8b92ffeb495
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-24 13:58:36 +0300

    add data

--- Gebeya/src/data.js
+++ Gebeya/src/data.js
@@ -1,6 +1,18 @@
 import French_kiss_bag from '../src/assets/French_kiss_bag.png';
 import Burbery_shine from '../src/assets/Burbery_shine.png'
 import Alvero_Gown from '../src/assets/Alvero_Gown.png';
+import shoes from '../src/assets/shoes.png';
+import bags from '../src/assets/bags.png';
+import jackets from '../src/assets/jackets.png';
+import lingerie from '../src/assets/lingerie.png';
+import belts from '../src/assets/belts.png';
+import street_wear from '../src/assets/street_wear.png';
+import sunglasses from '../src/assets/sunglasses.png';
+import sunwear from '../src/assets/sunwear.png';
+import hats from '../src/assets/hats.png';
+import slippers from '../src/assets/slippers.png';
+import wigs from '../src/assets/wigs.png';
+import cosmetics from '../src/assets/cosmetics.png';
 
 export const Latest_collections = [
     {
@@ -39,4 +51,8 @@ export const catagory_buttons = [
     { id: 4, text: "Perfumes" },
     { id: 5, text: "Sport" },
     { id: 6, text: "Jewelry" },
+];
+
+export const catagory_items = [
+    { id: 1, title: "Shoes", text:'See all collections',image:'' },
 ];
