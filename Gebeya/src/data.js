561451bf02d82360b09e893a0a07192e087b75af
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-24 10:57:29 +0300

    Finish Latest collection

---
+++ Gebeya/src/data.js
@@ -0,0 +1,33 @@
+import French_kiss_bag from '../src/assets/French_kiss_bag.png';
+import Burbery_shine from '../src/assets/Burbery_shine.png'
+import Alvero_Gown from '../src/assets/Alvero_Gown.png';
+
+export const Latest_collections = [
+    {
+        id: 1,
+        name: "French Kiss Bag",
+        image: French_kiss_bag,
+        price: 500,
+        brand: "ALDO",
+        category: "Women",
+        rating: "2k+"
+    },
+    {
+        id: 2,
+        name: "Burbery shine",
+        image: Burbery_shine,
+        price: 150,
+        brand: "FENDI",
+        category: "kids",
+        rating: "1k+"
+    },
+    {
+        id: 3,
+        name: "Alvero Gown",
+        image: Alvero_Gown,
+        price: 300,
+        brand: "DIVINE",
+        category: "Women",
+        rating: "5k+"
+    }
+];
--- Gebeya/src/pages/Home.jsx
+++ Gebeya/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { IoStar } from "react-icons/io5";
 import Ellipse_5 from '../assets/Ellipse_5.png';
 import Ellipse_3 from '../assets/Ellipse_3.png';
 import Ellipse_4 from '../assets/Ellipse_4.png';
-import French_kiss_bag from '../assets/French_kiss_bag.png';
+import { Latest_collections } from "../data";
 
 function Home() {
   return (
@@ -39,8 +39,8 @@ function Home() {
               <button className="bg-secondary text-white p-2 rounded-r-full"><FaAngleRight/></button>
             </div>
           </div>
-          <div id='1st_sub_section' className="grid grid-rows-3 pl-20">
-            <div className="max-w-[435px] max-h-[497px] border-2  p-4 rounded-2xl">
+          <div id='1st_sub_section' className="grid grid-cols-3 pl-20 pr-20 gap-4">
+            {/* <div className="max-w-[435px] max-h-[497px] border-2  p-4 rounded-2xl">
               <div className="flex flex-col gap-4 border-[#808080]">
                 <div className="relative bg-[#F2F2F2] items-center justify-center border rounded-2xl">
                   <img className="m-auto" src={French_kiss_bag} alt="French_kiss_bag" />
@@ -67,7 +67,41 @@ function Home() {
                   <button className="bg-secondary text-xl text-white h-[74px] p-3 rounded-l-2xl rounded-r-2xl">Add to cart</button>
                 </div>
               </div>
-            </div>
+            </div> */}
+            {Latest_collections.map((item)=>{
+              const { id, name, image, brand, rating, price, category } = item;
+              return(
+                <div key={id} className="max-w-[435px] max-h-[497px] border-2  p-4 rounded-2xl">
+                  <div className="flex flex-col gap-4 border-[#808080]">
+                    <div className="relative bg-[#F2F2F2] items-center justify-center border rounded-2xl">
+                      <img className="m-auto" src={image} alt={name} />
+                      <button className='absolute top-5 left-5 bg-white p-2 w-10 h-10 rounded-full flex items-center justify-center'><FaRegHeart/></button>
+                      <button className='bg-white p-2 w-[67px] h-[34px] items-center 
+                      justify-center border rounded-l-full rounded-r-full text-xs
+                      absolute top-5 right-5'>In Stock</button>
+                    </div>
+                    <div className="flex flex-row items-center justify-between">
+                      <h1 className="text-[#808080] border border-[#C1C1C1] rounded-l-full rounded-r-full p-1">{category}</h1>
+                      <h1 className="text-[#808080] flex flex-row items-center"><IoStar className="text-secondary"/>{rating} rating</h1>
+                    </div>
+                  </div>
+                  <div id='brand_and_price' className="flex flex-row justify-between">
+                    <div className="flex flex-col justify-between gap-10">
+                      <div className="flex flex-col gap-0">
+                        {/* <h1 className="text-2xl font-semibold">{name}</h1> */}
+                        <h1 className="text-[#808080] text-lg">{brand}</h1>
+                      </div>
+                      <h1 className="text-2xl font-semibold">${price}</h1>
+                    </div>
+                    <div className="flex flex-col justify-between">
+                      <div className=""></div>
+                      <button className="bg-secondary text-xl text-white h-[74px] p-3 rounded-l-2xl rounded-r-2xl">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+
           </div>
         </div>
     </div>
