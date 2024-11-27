3177d9ec050f51844ee55b0d65ae01f62ec4d569
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-27 17:45:32 +0300

    Checkout page in progress

--- Gebeya/src/App.jsx
+++ Gebeya/src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Cart from './pages/Cart'
-import SharedLayout from "./pages/SharedLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={ <SharedLayout/>}>
         <Route index element={<Home/>}/>
         <Route path="cart" element={<Cart/>}/>
+        <Route path="checkout" element={<Checkout/>}/>
         <Route path="*" element={<Error/>}/>
       </Route>
     </Routes>
--- Gebeya/src/pages/Checkout.jsx
+++ Gebeya/src/pages/Checkout.jsx
@@ -1,6 +1,19 @@
 function Checkout() {
   return (
-    <div>Checkout</div>
+    <div>
+      <div className=" flex flex-col p-4">
+        <h1 className='font-semibold text-2xl my-4'>Shipping Address</h1>
+        <h1 className="font-semibold text-lg">Name</h1>
+        <input type="text" placeholder="First & Last Name" className="border border-[#C1C1C1] rounded-lg h-14 p-2"/>
+        <h1 className="font-semibold text-lg mt-3">Address 1</h1>
+        <input type="text" placeholder="5kilo Addis Ababa, Ethiopia" className="border border-[#C1C1C1] rounded-lg h-14 p-2"/>
+        <h1 className="font-semibold text-lg mt-3">Address 2</h1>
+        <input type="text" placeholder="Addis Ababa University" className="border border-[#C1C1C1] rounded-lg h-14 p-2"/>
+      </div>
+      <div className="">
+
+      </div>
+    </div>
   )
 }
 export default Checkout
